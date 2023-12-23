e21cf3b1c6437120f28bc3179f162e87d95129b3
PandiiPT3 <129112944+PandiiPT3@users.noreply.github.com>
2023-12-23 20:35:35 +0000

    Add files via upload

--- index.js
+++ index.js
@@ -1,16 +1,3 @@
-// Get the height of the screen
-        var screenHeight = window.innerHeight;
-        var screenWidth=window.innerWidth;
-        var tempDiv = document.getElementById("tempDiv");
-        var bgHeight = tempDiv.clientHeight; // Altura da imagem de fundo
-        var bgWidth = tempDiv.clientWidth; // Altura da imagem de fundo
-        
-        var x = screenHeight/2 - bgWidth/2;
-
-// Set the background position based on screen height
-        document.body.style.backgroundPositionY = screenHeight/2 + bgHeight + "px";
-        document.body.style.backgroundPositionX =  screenWidth/2 - 130 + "px";
-        
 
 document.getElementById("vem-e-segue-me").addEventListener("click", function() {
     window.location.href = "https://www.churchofjesuschrist.org/study/come-follow-me?lang=ept";
@@ -36,5 +23,4 @@ document.getElementById("jovens-adultos").addEventListener("click", function() {
 document.getElementById("guia-para-os-jovens").addEventListener("click", function() {
     window.location.href =" https://www.churchofjesuschrist.org/study/manual/personal-development-youth-guidebook?lang=ept";
 });
-
 
