8d8620eb71246b5f730b477bc48d127085389340
PandiiPT3 <129112944+PandiiPT3@users.noreply.github.com>
2023-12-23 18:00:43 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+document.getElementById("vem-e-segue-me").addEventListener("click", function() {
+    window.location.href = "https://www.churchofjesuschrist.org/study/come-follow-me?lang=ept";
+});
+
+document.getElementById("liahona").addEventListener("click", function() {
+    window.location.href = "https://www.churchofjesuschrist.org/study/magazines/liahona?lang=ept";
+});
+
+document.getElementById("para-o-vigor-da-juventude").addEventListener("click", function() {
+    window.location.href = "https://www.churchofjesuschrist.org/study/magazines/for-the-strength-of-youth?lang=ept";
+});
+
+document.getElementById("amigo").addEventListener("click", function() {
+    window.location.href = " https://www.churchofjesuschrist.org/study/magazines/friend?lang=ept";
+});
+
+
+document.getElementById("jovens-adultos").addEventListener("click", function() {
+    window.location.href = "https://www.churchofjesuschrist.org/study/magazines/ya-weekly?lang=ept";
+});
+
+document.getElementById("guia-para-os-jovens").addEventListener("click", function() {
+    window.location.href =" https://www.churchofjesuschrist.org/study/manual/personal-development-youth-guidebook?lang=ept";
+});
+
+
