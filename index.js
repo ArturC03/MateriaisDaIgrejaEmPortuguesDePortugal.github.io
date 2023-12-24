ccef53e9e389325036f8d5a6799867a7a6663a17
PandiiPT3 <129112944+PandiiPT3@users.noreply.github.com>
2023-12-24 12:53:59 +0000

    Add files via upload

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ document.getElementById("para-o-vigor-da-juventude").addEventListener("click", f
 });
 
 document.getElementById("amigo").addEventListener("click", function() {
-    window.location.href = " https://www.churchofjesuschrist.org/study/magazines/friend?lang=ept";
+    window.location.href = "https://www.churchofjesuschrist.org/study/magazines/friend?lang=ept";
 });
 
 
@@ -21,6 +21,32 @@ document.getElementById("jovens-adultos").addEventListener("click", function() {
 });
 
 document.getElementById("guia-para-os-jovens").addEventListener("click", function() {
-    window.location.href =" https://www.churchofjesuschrist.org/study/manual/personal-development-youth-guidebook?lang=ept";
+    window.location.href ="https://www.churchofjesuschrist.org/study/manual/personal-development-youth-guidebook?lang=ept";
 });
 
+document.getElementById("ajuda-para-as-presidencias").addEventListener("click", function() {
+    window.location.href ="https://www.churchofjesuschrist.org/study/youth/helps-for-presidencies?lang=ept";
+});
+
+document.getElementById("tema-dos-jovens").addEventListener("click", function() {
+    window.location.href ="https://www.churchofjesuschrist.org/study/youth/themes?lang=ept";
+});
+
+document.getElementById("conferencia-geral").addEventListener("click", function() {
+    window.location.href ="https://www.churchofjesuschrist.org/study/general-conference?lang=ept";
+});
+
+document.getElementById("guia-para-as-criancas").addEventListener("click", function() {
+    window.location.href ="https://www.churchofjesuschrist.org/study/manual/personal-development-childrens-guidebook?lang=ept";
+});
+
+
+document.getElementById("devocionais-de-natal").addEventListener("click", function() {
+    window.location.href ="https://www.churchofjesuschrist.org/study/jesus-christ/christmas-videos/christmas-devotionals?lang=ept";
+});
+
+document.getElementById("proclamacao-da-restauracao").addEventListener("click", function() {
+    window.location.href ="https://www.churchofjesuschrist.org/study/scriptures/the-restoration-of-the-fulness-of-the-gospel-of-jesus-christ/a-bicentennial-proclamation-to-the-world?lang=ept";
+});
+
+// https://www.churchofjesuschrist.org/study/jesus-christ/christmas-videos/christmas-devotionals?lang=ept
